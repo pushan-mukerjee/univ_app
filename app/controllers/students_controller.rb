d81bdc90f20924e0667cec9ddb415475984de258
pushan-mukerjee <pushan.mukerjee@gmail.com>
2021-09-19 16:23:37 +1000

    students form errors partials and before action

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,5 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: [:show, :edit, :update]
 
   def index
     @students = Student.all
@@ -12,31 +13,34 @@ class StudentsController < ApplicationController
     @student = Student.new(student_params)
     if @student.save
       flash[:success] = "You have successfully signed up"
-      redirect_to root_path
+      redirect_to @student #same as redirect_to student_path(@student)
     else
       render 'new'
     end
   end
 
   def show
-    @student = Student.find(params[:id])
+
   end
 
   def edit
-    @student = Student.find(params[:id])
+
   end
 
   def update
-    @student = Student.find(params[:id])
     if @student.update(student_params)
       flash[:success] = "You have successfully updated your profile"
-      redirect_to @student #same as redirect_to student_path(@student)
+      redirect_to @student
     else
       render 'edit'
     end
   end
 
   private
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
 
   def student_params
     params.require(:student).permit(:name, :email)
