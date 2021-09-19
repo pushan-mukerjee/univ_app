ae623602a8dfeeb7863d5d8c817a9a4c8de76ea2
pushan-mukerjee <pushan.mukerjee@gmail.com>
2021-09-19 14:21:14 +1000

    Student index and new page with crud

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'courses#index'
   get 'about', to: 'pages#about'
   get 'courses/new', to: 'courses#new'
+  resources :students, except: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
