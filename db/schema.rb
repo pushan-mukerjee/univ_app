647e0109cfcec02a81621b57e669d0185438121d
pushan-mukerjee <pushan.mukerjee@gmail.com>
2021-09-19 17:08:46 +1000

    password and authentication functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_19_010146) do
+ActiveRecord::Schema.define(version: 2021_09_19_063058) do
 
   create_table "courses", force: :cascade do |t|
     t.string "short_name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_09_19_010146) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
