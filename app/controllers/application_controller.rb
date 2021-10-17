f43cff8dcddd1f1adf12ebbef219bf6c82d1c036
pushan-mukerjee <pushan.mukerjee@gmail.com>
2021-10-17 23:01:45 +1100

    login logout functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+   protect_from_forgery with: :exception
+
+   #defining helper_method enables all views to use the logged_in functionality
+   helper_method :current_user, :logged_in?
+
+   def current_user
+     #if session variable is set, it finds student based on student id in the session. The ||= means its on;ly executes database query if current user doesn't exist.
+     #if current user does exist, it just gets the @current_user already assigned and uses it in the logged_in def
+     @current_user ||= Student.find(session[:student_id]) if session[:student_id]
+   end
+
+   def logged_in?
+     !!current_user #returns true if current_user exists, esle returns false !! is true/false syntax in Rails.
+   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'courses/new', to: 'courses#new'
   resources :students, except: [:destroy]
+  get 'login', to: 'logins#new'
+  post 'login', to: 'logins#create'
+  delete 'logout', to: 'logins#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
